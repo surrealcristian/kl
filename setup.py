c43d78be0cfde5a082c5341e129f664759b64a91
surrealcristian <surrealcristian@gmail.com>
2015-07-12 20:52:22 -0300

    Version 3.0.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = []
 
 setup(
     name='kl',
-    version='2.1.0',
+    version='3.0.0',
     description='Simple keylogger for Linux + X11',
     author='Cristian Cabrera',
     author_email='surrealcristian@gmail.com',
