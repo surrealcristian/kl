768b0ecad5cba8fe416d65505e8171bb6398c187
surrealcristian <surrealcristian@gmail.com>
2015-07-13 23:48:56 -0300

    Tests

--- tests/test_kl.py
+++ tests/test_kl.py
@@ -8,7 +8,9 @@ def test_get_keys_empty_keymap():
     assert kl.get_keys(keymap) == dict(modifiers=[], regular=[])
 
 
-def test_get_keys_keymap_with_one_regular_keys():
-    keymap = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-              0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    assert kl.get_keys(keymap) == dict(modifiers=[], regular=['q'])
+def test_get_keys_keymap_with_modifiers_and_regular_keys():
+    keymap = [0, 0, 0, 0, 32, 1, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    regular = ['d']
+    modifiers = ['left ctrl', 'left shift']
+    assert kl.get_keys(keymap) == dict(modifiers=modifiers, regular=regular)
